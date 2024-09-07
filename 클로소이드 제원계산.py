86f4e1637e9a50b094f722c9a21b4460c9d6ba06
navya099 <130723074+navya099@users.noreply.github.com>
2024-09-07 13:30:19 +0900

    Update 클로소이드 제원계산.py
    완화곡선장 계산식변경

--- 클로소이드 제원계산.py
+++ 클로소이드 제원계산.py
@@ -1,4 +1,12 @@
+
 import math
+import numpy as np
+from scipy.optimize import fsolve
+
+# Define the function for the equation L
+def equation(L, R, S):
+    return L - np.sqrt((24 * R * S) / (1 - (L**2 / (112 * R**2)) + (L**4 / (2112 * R**4))))
+
 
 #입력 파라메터
 
@@ -8,6 +16,7 @@ AA = 2.66 #곡선구간 A'
 D = 4.4 #직선구간 선로중심간격
 L= 68.85 #완화곡선길이
 
+'''
 C = 0.076
 THETA = math.atan(C / 1.5)
 print(math.degrees(THETA))
@@ -19,9 +28,10 @@ F = HH * C / 1.5
 print(F)
 print(W)
 print(S)
+'''
 
 #상수
-digit = 3#반올림 자릿수
+digit = 32#반올림 자릿수
 
 #R1계산
 RR = R + D/2 #R'
@@ -34,13 +44,23 @@ Ro= round((R +(D/2))-So, digit)
 #내측궤도중심선
 Ri= round((Ro -(B + AA)), digit)
 Si = round((B + AA) - D + So,digit)
-Li = round(math.sqrt(24 * Ri * Si),3)
+# Li 계산 (순서 변경)
+# Initial guess for L
+L_initial_guess = L
+
+# Solve for L
+Li = fsolve(equation, L_initial_guess, args=(Ri, Si))
+Li_value = Li[0]  # Extract the scalar value
+Li_value = round(Li_value, digit)  # Rounding to precision
 
 #구축중심선
 Rc= round((Ro - AA ), digit)
 Sc = round((B + AA) - D + So,digit)
-Lc = round(math.sqrt(24 * Rc * Sc),3)
+Lc = fsolve(equation, L_initial_guess, args=(Rc, Sc))
+Lc_value = Lc[0]  # Extract the scalar value
+Lc_value = round(Lc_value, digit)  # Rounding to precision
 
-print(f'외측궤도 R= {Ro},구축중심 R= {Rc},내측궤도 R=  {Ri}') 
-print(f'외측궤도 L= {L},구축중심 L= {Lc},내측궤도 L=  {Li}') 
+print(f'외측궤도 R= {Ro:.3f},구축중심 R= {Rc:.3f},내측궤도 R=  {Ri:.3f}') 
+# Print results
+print(f'외측궤도 L = {L:.3f}, 구축중심 L = {Lc_value:.3f}, 내측궤도 L = {Li_value:.3f}')
     
