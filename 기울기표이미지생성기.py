d9c8cab61cdc6e11889e45ec415678323da052d1
navya099 <130723074+navya099@users.noreply.github.com>
2025-02-14 17:42:12 +0900

    Update 기울기표이미지생성기.py
    기울기표이미지생성함수 수정 클래스화

--- 기울기표이미지생성기.py
+++ 기울기표이미지생성기.py
@@ -341,49 +341,97 @@ class TextImageCreator:
             back_im.save(save_path)
             
 #기울기표용
-def create_text_image3(text, text2 , bg_color, filename, text_color, image_size=(500, 300), font_size=40):
-    # 이미지 생성
-    img = Image.new('RGB', image_size, color=bg_color)
-    draw = ImageDraw.Draw(img)
-    
-    # 폰트 설정
-    font = ImageFont.truetype("gulim.ttc", 150)
-    # 폰트 설정
-    font2 = ImageFont.truetype("gulim.ttc", 60)
-    # 텍스트 박스 크기 (25px 여백 적용)
-    box_x1, box_y1 = 25, 25
-    box_x2, box_y2 = image_size[0] - 25, image_size[1] - 25
-    box_width = box_x2 - box_x1
-    box_height = box_y2 - box_y1
+class GradePost:
+    def __init__(self, work_directory='c:/temp/pitch'):
+        self.work_directory = work_directory
+        if not os.path.exists(self.work_directory):
+            os.makedirs(self.work_directory)
 
-    # 줄바꿈 적용
-    wrapped_text = textwrap.fill(text, width=15)  # 글자 수 제한
-    text_bbox = draw.textbbox((0, 0), wrapped_text, font=font)
-    text_width = text_bbox[2] - text_bbox[0]
-    text_height = text_bbox[3] - text_bbox[1]
+    def create_text_image(self, text, font, text_color, image_size, rotate_angle=0):
+        text_image = Image.new('RGBA', image_size, (255, 255, 255, 0))
+        text_draw = ImageDraw.Draw(text_image)
+        text_draw.text((20, 20), text, font=font, fill=text_color)
+        rotated_text_image = text_image.rotate(rotate_angle, expand=True)
+        bbox = rotated_text_image.getbbox()
+        cropped_temp_img = rotated_text_image.crop(bbox) if bbox else rotated_text_image
+        white_bg = Image.new('RGB', cropped_temp_img.size, (255, 255, 255))
+        white_bg.paste(cropped_temp_img, (0, 0), cropped_temp_img.split()[3])
+        return white_bg
 
-    # 폰트 크기가 박스를 넘으면 자동 조정
-    while text_width > box_width or text_height > box_height:
-        font_size -= 2
-        font = ImageFont.truetype("gulim.ttc", font_size)
-        text_bbox = draw.textbbox((0, 0), wrapped_text, font=font)
-        text_width = text_bbox[2] - text_bbox[0]
-        text_height = text_bbox[3] - text_bbox[1]
+    def create_arrow_symbol(self, image_size, text_color, is_negative):
+        arrow_image = Image.new('RGBA', image_size, (255, 255, 255, 0))
+        arrow_draw = ImageDraw.Draw(arrow_image)
+        arrow_points = [(372, 70), (417, 42), (401, 15), (456, 38), (458, 98), (440, 73), (393, 104), (372, 70)]
+        arrow_draw.polygon(arrow_points, fill=text_color, outline=text_color)
+        return arrow_image.transpose(Image.FLIP_TOP_BOTTOM) if is_negative else arrow_image
 
-    
-    #구배문자
-    draw.text((20, 0), text, font=font, fill=text_color , rotate= 25)
+    def create_grade_post(self, text, text2, filename, text_color, post_direction, image_size=(500, 400), font_size1=180, font_size2=100):
+        img = Image.new('RGB', image_size, (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+        try:
+            font_main = ImageFont.truetype('c:/windows/fonts/HYGTRE.ttf', font_size1)
+            font_sub = ImageFont.truetype("gulim.ttc", font_size2)
+        except IOError:
+            print("폰트 파일을 찾을 수 없습니다.")
+            return
 
-    #구간거리
+        is_negative = text.startswith('-')
+        integer_part, decimal_part = text.lstrip('-').split('.') if '.' in text else (text.lstrip('-'), None)
+        rotate_angle = -30 if is_negative else 30
+        if text == '0':
+            white_bg = img
+        else:
+            white_bg = self.create_text_image(integer_part, font_main, text_color, image_size, rotate_angle)
+        post_positions = {'좌': (107, 70 if int(integer_part) > 9 else 110), '우': (110, 110)}
+        img.paste(white_bg, post_positions.get(post_direction, (110, 110)))
 
-    draw.text((20, 250), f'{text2}M', font=font2, fill=text_color)
-    
-    # 이미지 저장
-    if not filename.endswith('.png'):
-        filename += '.png'
-    final_dir = work_directory + filename
-    img.save(final_dir)
+        if decimal_part:
+            font_decimal = ImageFont.truetype('c:/windows/fonts/HYGTRE.ttf', 75)
+            decimal_bg = self.create_text_image(decimal_part, font_decimal, text_color, image_size, rotate_angle)
+            decimal_positions = self.get_decimal_position(post_direction, integer_part, is_negative)
+            img.paste(decimal_bg, decimal_positions)
         
+        draw.line([(0, 280), (500, 280)], fill=text_color, width=10)
+        distance_positions = self.get_distance_position(post_direction, text2)
+        distance_text = f'L={text2}M' 
+        draw.text(distance_positions, distance_text, font=font_sub, fill=text_color)
+        if text == '0':
+            arrow_image = self.create_L_symbol(image_size, text_color)
+        else:
+            arrow_image = self.create_arrow_symbol(image_size, text_color, is_negative)
+        
+        img.paste(arrow_image, (0, -120 if is_negative else 0), arrow_image)
+
+        if not filename.endswith('.png'):
+            filename += '.png'
+        final_dir = os.path.join(self.work_directory, filename)
+        img.save(final_dir)
+        #print(f"최종 이미지 저장됨: {final_dir}")
+        
+    def get_decimal_position(self, post_direction, integer_part, is_negative):
+        if post_direction == '좌':
+            if int(integer_part) > 9:#구배가 2자리
+                return (330, 140) if is_negative else (310, 40)
+            else:#구배가 1자리
+                return (240, 160) if is_negative else (200, 60)
+        return (110, 110)  # '우' 기본 위치
+    
+    def get_distance_position(self, post_direction, integer_part):
+        if post_direction == '좌':
+            
+            if len(integer_part) == 3:#거리가 3자리
+                return (100,290)
+            elif len(integer_part) == 4:#거리가 4자리
+                return (75,290)
+            else:
+                return (60,290)
+    def create_L_symbol(self, image_size, text_color):
+        arrow_image = Image.new('RGBA', image_size, (255, 255, 255, 0))
+        arrow_draw = ImageDraw.Draw(arrow_image)
+        arrow_points = [(121, 42), (121, 242), (313, 242), (313, 181), (171, 181), (171, 42)]
+        arrow_draw.polygon(arrow_points, fill=text_color, outline=text_color)
+        return arrow_image
+    
 def copy_and_export_csv(open_filename='SP1700', output_filename='IP1SP',isSPPS = False, R= 3100, curvetype='SP'):
     # Define the input and output file paths
     open_file = work_directory + open_filename + '.csv'
@@ -631,7 +679,7 @@ else:
     text_color = (0,0,0)
     structure_comment = []
     creator = TextImageCreator(work_directory=work_directory, font_path="gulim.ttc", font_size=60)
-    
+    grade_post_generator = GradePost()
     for i, section in enumerate(annotated_sections, start=1):
         for line in section:
             #VIP별 기울기 추출
@@ -774,17 +822,17 @@ else:
                     creator.create_image(img_bg_color,(345,200),station_text, R_text, pitchtype, isSagCrest, (0,0,0), img_f_name)
                     
                     #기울기표
-                    img_text2 = f'{current_grade}'#기울기표 구배문자
-                    img_text3 = f'L={current_distance}' #기울기표 거리문자                    
+                    img_text2 = format_grade(current_grade)#기울기표 구배문자
+                    img_text3 = f'{current_distance}' #기울기표 거리문자                    
                     img_bg_color2 = (255, 255, 255) #기울기표 문자                     
                     img_f_name2 = f'VIP{i}_{pitchtype}_기울기표'#기울기표 파일명
                     openfile_name2 = f'기울기표_{structure}용'
                     
                     
                     #기울기표 출력
-                    create_text_image3(img_text2, img_text3, img_bg_color2, img_f_name2, text_color, image_size=(500, 400), font_size=40)
+                    #create_text_image3(img_text2, img_text3, img_bg_color2, img_f_name2, text_color, image_size=(500, 400), font_size=40)
                     #create_text_image(station_text, f'{R}',  pitchtype, isSagCrest , img_bg_color, img_f_name, text_color, image_size=(345, 200), font_size=60)
-
+                    grade_post_generator.create_grade_post(img_text2, img_text3, img_f_name2, (0, 0, 0), '좌')
                 else:
                     print('에러')
                     station_text = '2'
