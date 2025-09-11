34d38815584c5d2cecd9747b4277af2b15d1f9e8
navya099 <qltmxkr@gmail.com>
2025-09-11 17:58:30 +0900

    Update dxfmanager.py
    ARC 그리기 추가

--- BVEAlignmentViewer/fileexporter/dxfmanager.py
+++ BVEAlignmentViewer/fileexporter/dxfmanager.py
@@ -1,7 +1,6 @@
 import ezdxf
 
-from model.model import IPdata
-
+from model.model import IPdata, CurveType, CurveDirection
 
 class DXFController:
     def __init__(self):
@@ -14,9 +13,53 @@ class DXFController:
 
         # IP라인 그리기
         self._draw_ipline(ipdata)
+        # FL 그리기
+        self._draw_arc(ipdata)
         #저장
         self.doc.saveas(filepath)
 
     def _draw_ipline(self, ipdata: list[IPdata]):
         points = [(ip.coord.x, ip.coord.y) for ip in ipdata]
         lwpolyline = self.msp.add_lwpolyline(points)
+
+    def _draw_arc(self, ipdata: list[IPdata]):
+        """
+        ipdata를 바탕으로 호 객체를 그리는 메소드
+        Args:
+            ipdata:
+
+        Returns:
+            msp.arc
+        """
+        for i, ip in enumerate(ipdata):
+            if i ==0:
+                #BP 생략
+                continue
+            elif i== len(ipdata)-1:
+                #EP 생략
+                continue
+            else:
+                #ip내부 호 객체 요소 추출
+                if ip.curvetype == CurveType.Simple:
+                    for seg in ip.segment:
+                        center = seg.center_coord
+                        start = seg.start_coord
+                        end = seg.end_coord
+                        r = seg.radius
+
+                        start_angle = angle_from_center(center, start)
+                        end_angle = angle_from_center(center, end)
+
+                        # 왼쪽/오른쪽 호 처리
+                        if ip.curve_direction == CurveDirection.RIGHT:
+                            start_angle, end_angle = end_angle, start_angle
+
+                        self.msp.add_arc(
+                            center=(center.x, center.y),
+                            radius=r,
+                            start_angle=start_angle,
+                            end_angle=end_angle,
+                            dxfattribs={'layer': 'FL'}
+                        )
+                else:
+                    continue
