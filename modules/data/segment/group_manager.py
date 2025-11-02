390e806553a719700898961f1d9d974d52615f83
navya099 <130723074+navya099@users.noreply.github.com>
2025-11-02 09:55:45 +0900

    Update group_manager.py
    find_group_near_coord 추가

--- modules/data/segment/group_manager.py
+++ modules/data/segment/group_manager.py
@@ -1,4 +1,5 @@
 from data.segment.segment_group import SegmentGroup
+from data.segment.straight_segment import StraightSegment
 
 
 class GroupManager:
@@ -8,3 +9,14 @@ class GroupManager:
     def create_curve_group(self, i, bp, ip, ep, r, isspiral) -> SegmentGroup | None:
 
         return SegmentGroup.create_from_pi(i, bp, ip, ep, r, isspiral)
+
+    def find_group_near_coord(self, coord):
+        """
+        주어진 PI 좌표가 영향을 미치는 그룹을 반환.
+        (BP~EP 구간 안에 있는 그룹)
+        """
+        for group in self.groups:
+            if group.ip_coordinate == coord:
+                return group  # 완전 일치
+        return None
+
