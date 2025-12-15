fe91786bae6e6d84d18ca661b7a436cd8fe06deb
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-15 21:11:31 +0900

    Update 완화곡선구현테스트.py

--- 테스트/완화곡선구현테스트.py
+++ 테스트/완화곡선구현테스트.py
@@ -89,10 +89,6 @@ def test(bp,ip,ep,m:float,z:float,v:float,radius:float,sp_sta:float,target_sta:f
                 ys.append(p.y)
                 break
 
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(bottom=0.25)
-    ax.plot(xs, ys)
-
     print('\n완화곡선 제원출력\n')
     print(f'방위각1={90 - math.degrees(group.bp_azimuth)}')
     print(f'방위각2={90 - math.degrees(group.ep_azimuth)}')
@@ -120,7 +116,23 @@ def test(bp,ip,ep,m:float,z:float,v:float,radius:float,sp_sta:float,target_sta:f
     print(f'PS={cal.end_transition.x},{cal.end_transition.y}')
     print(f'\n임의 지점 {target_sta}의 좌표 및 접선 방위각=\nX={pt.x},Y={pt.y}, DR={90 - math.degrees(dr)}')
 
+    fig, ax = plt.subplots()
+    ax.scatter(cal.start_transition.x, cal.start_transition.y, c='b')
+    ax.scatter(cal.start_circle.x, cal.start_circle.y, c='b')
+    ax.scatter(cal.end_circle.x, cal.end_circle.y, c='b')
+    ax.scatter(cal.end_transition.x, cal.end_transition.y, c='b')
+    ax.scatter(pt.x, pt.y, c='r', marker='x')
+
+    ax.scatter(ip.x, ip.y, c='g')
+    ax.plot(
+        [bp.x, ip.x, ep.x],
+        [bp.y, ip.y, ep.y],
+        c='b'
+    )
+    curve_plot, = ax.plot(xs, ys, color='red')
+    ax.set_aspect('equal')
     plt.show()
+
 bp=Point2d(217366.8274,433678.2264)
 ip=Point2d(218848.2178, 434729.9024)
 ep=Point2d(220748.6982,433879.1276)
