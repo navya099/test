3c357f2e0f47d73b65490f8e7dd8bfa3ea157c41
navya099 <qltmxkr@gmail.com>
2025-12-18 14:33:35 +0900

    Update segment.py
    메서드 설명 독스트링 변경

--- modules/data/segment/segment.py
+++ modules/data/segment/segment.py
@@ -26,40 +26,40 @@ class Segment(ABC):
 
     @abstractmethod
     def distance_to_point(self, point: Point2d) -> float:
-        """추상메서드 세그먼트와 점의 거리 계산"""
+        """세그먼트와 점의 거리 계산"""
         pass
 
     @abstractmethod
     def point_at_station(self, station: float, offset: float) -> tuple[Point2d, float]:
-        """추상메서드 지정한 측점의 좌표와 방위각 반환"""
+        """지정한 측점의 좌표와 방위각 반환"""
         pass
 
     @abstractmethod
     def station_at_point(self, coord: Point2d) -> tuple[float, float]:
-        """추상메서드 지정한 좌표의 측점 및 거리 반환"""
+        """지정한 좌표의 측점 및 거리 반환"""
         pass
 
     @abstractmethod
     def is_contains_station(self, station: float) -> bool:
-        """추상메서드 지정한 측점이 세그먼트에 포함되는지 여부"""
+        """지정한 측점이 세그먼트에 포함되는지 여부"""
         pass
 
     @abstractmethod
     def is_contains_point(self, point: Point2d) -> bool:
-        """추상메서드 지정한 점이 세그먼트에 포함되는지 여부"""
+        """지정한 점이 세그먼트에 포함되는지 여부"""
         pass
 
     @abstractmethod
     def reverse(self):
-        """추상메서드 세그먼트 뒤집기"""
+        """세그먼트 뒤집기"""
         pass
 
     @abstractmethod
     def create_offset(self, offset_distance: float) -> Self:
-        """추상메서드 세그먼트 객체의 평행(오프셋) 복제본을 생성"""
+        """세그먼트 객체의 평행(오프셋) 복제본을 생성"""
         pass
 
     @abstractmethod
     def split_to_segment(self, coord: Point2d) -> Self:
-        """추상메서드 지점한 점으로 세그먼트 객체 분할"""
+        """지점한 점으로 세그먼트 객체 분할"""
         pass
