f934a988d69f9d650f5d3a73162e529236286a5c
navya099 <qltmxkr@gmail.com>
2025-12-24 17:00:10 +0900

    Update spiral_geometry.py
    TransitionCurvatureCalculator에서 공통 인터페이스인 SpiralGeometry로 변경

--- modules/data/alignment/geomtry/spiral/spiral_geometry.py
+++ modules/data/alignment/geomtry/spiral/spiral_geometry.py
@@ -1,70 +1,82 @@
+from dataclasses import dataclass
+import math
 from AutoCAD.point2d import Point2d
 from curvedirection import CurveDirection
-from transitioncurveparams import TransitionCurveParams
-import math
+from data.alignment.geometry.segmentgeometry import SegmentGeometry
+from data.alignment.geometry.spiral.params import TransitionCurveParams
 
-class TransitionCurvatureCalculator:
-    """완화곡선 좌표 계산 클래스
-        Attributes:
-            params:완화곡선 파라메터
-            h1: ip시작 방위각(rad)
-            h2: ip끝 방위각(rad)
-            ip:IP좌표
-            dir: 방향
-            start_transition: 완화곡선 시작점
-            start_circle: 원곡선 시작점
-            end_circle: 원곡선 끝점
-            end_transition: 완화곡선 끝점
 
+@dataclass
+class SpiralGeometry(SegmentGeometry):
+    """완화곡선 지오메트리 엔진
+    Attributes:
+        direction: 곡선방향
+        params: 완화곡선 파라메터
     """
-    def __init__(self, tr_params: TransitionCurveParams, h1: float, h2: float, ip: Point2d, direction: CurveDirection):
-        self.params = tr_params
-        self.h1 = h1
-        self.h2 = h2
-        self.ip = ip
-        self.dir = direction
-        self.start_transition = None
-        self.start_circle = None
-        self.end_circle = None
-        self.end_transition = None
+    direction: CurveDirection = CurveDirection
+    params: TransitionCurveParams = TransitionCurveParams
 
-    def run(self):
-        self.cal_start_transition()
-        self.cal_end_transition()
-        self.cal_start_circle()
-        self.cal_end_circle()
+    @property
+    def a(self):
+        return self.params.a
 
-    def cal_start_transition(self):
-        """완화곡선 시작 좌표 계산 메서드"""
-        #ip에서 h1 역방향만큼 tl만큼 이동한 점
-        point = self.ip.moved(self.h1 + math.pi, self.params.total_tangent_length)
-        self.start_transition = point
+    @property
+    def delta(self):
+        """완화곡선 전체 각도"""
+        return self.params.theta_pc
 
-    def cal_end_transition(self):
-        """완화곡선 끝 좌표 계산 메서드"""
-        #ip에서 h2만큼 tl만큼 이동한 점
-        point = self.ip.moved(self.h2, self.params.total_tangent_length)
-        self.end_transition = point
+    @property
+    def total_x(self):
+        """완화곡선 전체 X"""
+        return self.params.x1
 
-    def cal_start_circle(self):
-        """원곡선 시작 좌표 계산 메서드"""
-        #start_transition에서 x1만큼 이동
-        point = self.start_transition.moved(self.h1, self.params.x1)
-        #y1만큼 이동(방향에 따라 +-)
-        if self.dir == CurveDirection.LEFT:
-            point.move(self.h1 + math.pi /2, self.params.y1)
+    @property
+    def total_y(self):
+        """완화곡선 전체 Y"""
+        return self.params.y1
+
+    def tangent_at(self, s: float) -> float:
+        return self.local_tangent(s)
+
+    def point_at(self, s: float, offset: float = 0.0) -> Point2d:
+        return self.local_xy(s)
+
+    def project_at(self, p: Point2d) -> tuple[Point2d, float]:
+        pass
+
+    def distance_at(self, p: Point2d) -> float:
+        pass
+
+    def reversed(self):
+        pass
+
+    def get_offset(self, offset: float):
+        pass
+
+    def local_xy(self, s: float) -> Point2d:
+        """완화곡선 로컬좌표 (x, y) 계산"""
+        r = self.params.radius
+        l = self.params.l
+        x1 = self.params.x1
+
+        x = s - (s**5) / (40 * r * r * (l**2))
+        y = (x**3) / (6 * r * x1) if x1 != 0 else 0
+
+        return Point2d(x, y)
+
+    def local_tangent(self, s):
+        pt = self.local_xy(s)
+
+        r = self.params.radius
+        l = self.params.l
+        x1 = self.params.x1
+
+        x = pt.x
+        dx = 1 - (s ** 4) / (8 * r * r * l * l)
+
+        if x1 != 0:
+            dy = (x * x * dx) / (2 * r * x1)
         else:
-            point.move(self.h1 - math.pi / 2, self.params.y1)
-        self.start_circle = point
+            dy = 0
 
-    def cal_end_circle(self):
-        """원곡선 종점 좌표 계산 메서드"""
-        # end_transition에서 x1만큼 이동
-        point = self.end_transition.moved(self.h2 + math.pi, self.params.x1)
-        # y1만큼 이동(방향에 따라 +-)
-        if self.dir == CurveDirection.LEFT:
-            point.move(self.h2 + math.pi / 2, self.params.y1)
-        else:
-            point.move(self.h2 - math.pi / 2, self.params.y1)
-
-        self.end_circle = point
+        return math.atan2(dy, dx)
