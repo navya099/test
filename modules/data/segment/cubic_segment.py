1b250aaf6788f851ec9504a845b27d9754b49a34
navya099 <qltmxkr@gmail.com>
2025-12-24 16:47:22 +0900

    Update cubic_segment.py
    기존 pointcal의 내용을 옮김 및 공통 인터페이스로 통일

--- modules/data/segment/cubic_segment.py
+++ modules/data/segment/cubic_segment.py
@@ -1,30 +1,34 @@
 from dataclasses import dataclass, field
-from AutoCAD.point2d import Point2d
-from data.alignment.spiral.pointcalc import SpiralPointCalculator
-from data.segment.segment import Segment
-from data.alignment.spiral.geometry import TransitionCurvatureCalculator
 import math
+from AutoCAD.point2d import Point2d
+from curvedirection import CurveDirection
+from data.alignment.geometry.spiral.params import TransitionCurveParams
+from data.segment.segment import Segment
+from data.alignment.geometry.spiral.spiral_geometry import SpiralGeometry
+
 
 @dataclass
 class CubicSegment(Segment):
     """
     3차포물선 완화곡선 세그먼트
     Attributes:
-        start_azimuth: 시각 각도
-        end_azimuth: 끝 각도
-        radius: 곡선반경
         isstarted: 시작 완화곡선 여부
-        start_coord: 시작 각도
-        end_coord: 끝 각도
         geom: 지오메트리 정보
+        params: 완화곡선 파라메터 정보
+        start_coord: 시작 좌표
+        end_coord: 끝 좌표
+        start_azimuth: 시작 각도
+        end_azimuth: 끝 각도
     """
+
+    isstarted: bool = False
+    geom: SpiralGeometry = SpiralGeometry
+    params: TransitionCurveParams = TransitionCurveParams
     start_azimuth: float = 0.0
     end_azimuth: float = 0.0
-    radius: float = 0.0
-    isstarted: bool = False
-    start_coord:  Point2d = field(default_factory=lambda: Point2d(0, 0))
-    end_coord:  Point2d = field(default_factory=lambda: Point2d(0, 0))
-    geom: TransitionCurvatureCalculator | None = None
+    start_coord: Point2d = field(default_factory=lambda: Point2d(0, 0))
+    end_coord: Point2d = field(default_factory=lambda: Point2d(0, 0))
+    direction: CurveDirection = CurveDirection.NULL
 
     @property
     def length(self):
@@ -36,54 +40,69 @@ class CubicSegment(Segment):
         """완화곡선 매개변수 A"""
         return self.geom.params.a
 
+    @property
+    def delta(self):
+        """완화곡선 접선각"""
+        return self.geom.params.theta_pc
+
     def distance_to_point(self, point: Point2d) -> float:
-        """
-        점과 원호(곡선 세그먼트) 사이의 최단 거리 계산
-        (Shapely 사용하지 않음, 순수 수학 기반)
-        """
         pass
 
     def point_at_station(self, station: float, offset: float = 0.0) -> tuple[Point2d, float]:
-        """
-        station 기반 좌표 계산
-        반환값: (Point2d, tangent_angle)
-        """
         # --- 1) Station 비율 계산 ---
         if station < self.start_sta or station > self.end_sta:
             raise ValueError("Station이 세그먼트 범위를 벗어남")
         s = station - self.start_sta
-        sp_calc = SpiralPointCalculator(self.geom.params, self.geom.dir)
         if self.isstarted:
-            theta = self.geom.h1
-            pt = sp_calc.global_xy(self.start_coord, theta, s)
-            azimuth = sp_calc.tangent_bearing(s, theta)
+            theta = self.start_azimuth #H1
+            pt = self.global_xy(s, self.start_coord, self.start_azimuth, isexit=False)
+            azimuth = self.global_tangent(s , self.start_azimuth, isexit=False)
         else:
-            theta = self.geom.h2
+            theta = self.end_azimuth #h2
             rev_s = self.length - s
-            pt = sp_calc.global_xy(self.end_coord, theta, rev_s,isexit=True)
-            azimuth = sp_calc.tangent_bearing(rev_s, theta, isexit=True)
+            pt = self.global_xy(rev_s, self.end_coord, theta, isexit=True)
+            azimuth = self.global_tangent(rev_s, theta, isexit=True)
         return pt, azimuth
 
     def station_at_point(self, coord: Point2d) -> tuple[float, float]:
-        """지정한 좌표의 측점 및 거리 반환"""
         pass
 
     def is_contains_station(self, station: float) -> bool:
-        """지정한 측점이 세그먼트에 포함되는지 여부"""
         pass
 
     def is_contains_point(self, point: Point2d) -> bool:
-        """지정한 점이 세그먼트에 포함되는지 여부"""
         pass
 
     def reverse(self):
-        """세그먼트 뒤집기"""
         pass
 
     def create_offset(self, offset_distance: float):
-        """세그먼트 객체의 평행(오프셋) 복제본을 생성"""
         raise NotImplementedError
 
     def split_to_segment(self, coord: Point2d):
-        """세그먼트 분할(완화곡선은 미지원"""
         raise NotImplementedError
+
+    #전역좌표
+    def global_xy(self, s, origin: Point2d, origin_az: float, isexit=False) -> Point2d:
+        """로컬좌표(x,y)를 글로벌 좌표로 변환하여 실제 점 반환"""
+        pt = self.geom.local_xy(s)
+        if isexit:
+            pt.x = -pt.x
+        if self.direction == CurveDirection.RIGHT:
+            pt.y = -pt.y
+
+        ca = math.cos(origin_az)
+        sa = math.sin(origin_az)
+
+        gx = origin.x + pt.x * ca - pt.y * sa
+        gy = origin.y + pt.x * sa + pt.y * ca
+
+        return Point2d(gx, gy)
+
+    #전역접선각
+    def global_tangent(self, s, origin_az: float, isexit=False) -> float:
+        t = self.geom.local_tangent(s)
+        if isexit:
+            t = -t
+
+        return origin_az + t
