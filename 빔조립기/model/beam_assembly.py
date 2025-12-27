cda71004bf70209b33dc61d39149107867014106
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-27 14:25:26 +0900

    Update beam_assembly.py
    _create_brackets 메서드 수정

--- 빔조립기/model/beam_assembly.py
+++ 빔조립기/model/beam_assembly.py
@@ -1,7 +1,4 @@
 from dataclasses import dataclass
-
-from Electric.Overhead.Pole.poletype import PoleType
-from Electric.Overhead.Structure.beamtype import BeamType
 from model.beam import Beam
 from model.bracket import Bracket
 from model.colum import Column
@@ -31,15 +28,7 @@ class BeamAssembly:
     @staticmethod
     def _create_beam(install, idxlib):
         length = abs(install.left_x) + abs(install.right_x)
-        if install.beam_type == BeamType.PIPE:
-            b_name = '깅관빔'
-        elif install.beam_type == BeamType.TRUSS:
-            b_name = '트러스빔'
-        elif install.beam_type == BeamType.TRUSS_RHAMEN:
-            b_name = '트러스라멘빔'
-        else:
-            b_name = '트러스빔'
-        name = f"{b_name}-{int(length)}m"
+        name = f"{install.beam_type}-{int(length)}m"
         index = idxlib.get_index(name)
 
         return Beam(
@@ -52,21 +41,13 @@ class BeamAssembly:
     def _create_columns(install, idxlib):
         columns = []
         xs = [install.left_x, install.right_x]
-        if install.pole_type == PoleType.PIPE.label:
-            pole_type = PoleType.PIPE.label
-        elif install.pole_type == PoleType.H_BEAM.label:
-            pole_type = PoleType.H_BEAM.label
-        elif install.pole_type == PoleType.STEEL.label:
-            pole_type = PoleType.STEEL.label
-        else:
-            pole_type = PoleType.PIPE.label
         for x in xs[:install.pole_count]:
-            name = f"{pole_type}-{install.pole_width}-{int(install.pole_height)}m"
+            name = f"{install.pole_type}-{install.pole_width}-{int(install.pole_height)}m"
             index = idxlib.get_index(name)
 
             columns.append(
                 Column(
-                    type=pole_type,
+                    type=install.pole_type,
                     width=install.pole_width,
                     xoffset=x,
                     yoffset=0.0,
@@ -79,24 +60,22 @@ class BeamAssembly:
     @staticmethod
     def _create_brackets(install, idxlib):
         brackets = []
-        for rail_name_var, rail_idx_var, bracket_var, x_var, y_var, rotate_var in install.brackets:
-            rail_name = rail_name_var.get()
-            rail_idx = rail_idx_var.get()
-            bracket = bracket_var.get()
-            x = x_var.get()
-            y = y_var.get()
-            rot = rotate_var.get()
-            bracket = bracket.replace('.csv', '')
-            index = idxlib.get_index(bracket)
 
-            brackets.append(
-                Bracket(
-                    rail_no=rail_name,
-                    type=bracket,
-                    xoffset=x,
-                    yoffset=y,
-                    rotation=rot,
-                    index=index
+        for rail in install.brackets:  # RailData 단위로 순회
+            for b in rail.brackets:  # 이미 Bracket 객체임
+                name = b.type
+                name = name.replace('.csv', '')
+                index = idxlib.get_index(name)
+
+                brackets.append(
+                    Bracket(
+                        rail_no=rail.index,
+                        type=name,
+                        xoffset=b.xoffset,
+                        yoffset=b.yoffset,
+                        rotation=b.rotation,
+                        index=index,
+                    )
                 )
-            )
+
         return brackets
