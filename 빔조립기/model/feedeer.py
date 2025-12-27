1794cc51f25814def4ff26328d1f13b12d1d4395
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-27 14:24:24 +0900

    Update feedeer.py
    rotation 속성 추가

--- 빔조립기/model/feedeer.py
+++ 빔조립기/model/feedeer.py
@@ -9,8 +9,10 @@ class Feederdata:
         index: 급전선 지지물 인덱스
         xoffset: 선로중심 기준 x 오프셋
         yoffset: 선로중심 기준 y 오프셋
+        rotation: 회전
         """
     type: str = ''
     index: int = 0
     xoffset: float = 0.0
     yoffset: float = 0.0
+    rotation: float = 0.0
