7271741c26782468a8c937db63ab4736c40dfd86
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-27 17:16:07 +0900

    Update kmprocessor.py
    코드 간결화

--- kmpostcreator/kmpost/kmprocessor.py
+++ kmpostcreator/kmpost/kmprocessor.py
@@ -1,30 +1,33 @@
+from infrastructure.dxftoimg import DXF2IMG
+from kmpost.lineprocessor import LineProcessor
+from model.bveimgdata import BVEImageData
+import os
+
 class KMPostProcessor:
-    """처리 클래스"""
+    """DXF 수정 및 이미지 변환"""
 
-    def __init__(self):
-        pass
+    @staticmethod
+    def process_dxf_image(
+        data: BVEImageData,
+        source_directory: str,
+        work_directory: str,
+        post_type: str,
+        alignmenttype: str
+    ):
+        file_path = os.path.join(source_directory, f'{post_type}.dxf')
+        modified_path = os.path.join(work_directory, f'{post_type}-수정됨.dxf')
 
-    def process_dxf_image(self, data: BVEImageData, source_directory: str, work_directory: str, post_type: str,
-                          alignmenttype: str):
-        """DXF 파일 수정 및 이미지 변환"""
-        file_path = source_directory + post_type + '.dxf'
-        modifed_path = work_directory + post_type + '-수정됨.dxf'
+        lineprogram = LineProcessor(file_path, modified_path, data, alignmenttype)
+        mode = 'km' if post_type == 'km표' else 'm'
+        lineprogram.replace_text_in_dxf(mode=mode)
 
-        lineprogram = LineProcessor(file_path, modifed_path, data, alignmenttype)
-        if post_type == 'km표':
-            lineprogram.replace_text_in_dxf(mode='km')
-
-        else:
-            lineprogram.replace_text_in_dxf(mode='m')
-
-        # 이미지 추출
-        final_output_image = os.path.join(work_directory, data.imgname + '.png')
         converter = DXF2IMG()
-        if alignmenttype == '도시철도':
-            target_size = (200, 250)
-        else:
-            target_size = (180, 650)
-        output_paths = converter.convert_dxf2img([modifed_path], img_format='.png')
+        output_paths = converter.convert_dxf2img([modified_path], img_format='.png')
 
-        if output_paths:
-            converter.trim_and_resize_image(output_paths[0], final_output_image, target_size)
+        if not output_paths:
+            return
+
+        final_image = os.path.join(work_directory, f'{data.imgname}.png')
+
+        target_size = (200, 250) if alignmenttype == '도시철도' else (180, 650)
+        converter.trim_and_resize_image(output_paths[0], final_image, target_size)
