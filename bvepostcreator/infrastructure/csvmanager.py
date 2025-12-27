332087b9f98a2fd22e74543b0c7668eb36b968a5
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-27 18:23:22 +0900

    Update csvmanager.py
    copy_and_export_csv 구현

--- bvepostcreator/infrastructure/csvmanager.py
+++ bvepostcreator/infrastructure/csvmanager.py
@@ -1,29 +1,30 @@
+import os
+
 class CSVManager:
-    def __init__(self):
-        pass
+    @staticmethod
+    def copy_and_export_csv(
+        open_filename: str,
+        output_filename: str,
+        ptype: str,
+        source_directory: str,
+        work_directory: str,
+        offset: float = 0.0,
+    ):
+        open_file = os.path.join(source_directory, f'{open_filename}.csv')
+        output_file = os.path.join(work_directory, f'{output_filename}.csv')
 
-    def copy_and_export_csv(open_filename='km표-토공용', output_filename='13460', ptype='km표', source_directory='',
-                            work_directory='', offset=0.0):
-        # Define the input and output file paths
-        open_file = source_directory + open_filename + '.csv'
-        output_file = work_directory + output_filename + '.csv'
-
-        # List to store modified lines
         new_lines = []
 
-        # Open the input file for reading
-        with open(open_file, 'r', encoding='utf-8') as file:
-            # Iterate over each line in the input file
-            for line in file:
-                # Replace 'LoadTexture, SP.png,' with 'LoadTexture, output_filename.png,' if found
+        with open(open_file, 'r', encoding='utf-8') as f:
+            for line in f:
                 if f'LoadTexture, {ptype}.png,' in line:
-                    line = line.replace(f'LoadTexture, {ptype}.png,', f'LoadTexture, {output_filename}.png,')
-
-                # Append the modified line to the new_lines list
+                    line = line.replace(
+                        f'LoadTexture, {ptype}.png,',
+                        f'LoadTexture, {output_filename}.png,'
+                    )
                 new_lines.append(line)
+
         new_lines.append(f'\nTranslateAll, {offset}, 0, 0\n')
 
-        # Open the output file for writing the modified lines
-        with open(output_file, 'w', encoding='utf-8') as file:
-            # Write the modified lines to the output file
-            file.writelines(new_lines)
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.writelines(new_lines)
