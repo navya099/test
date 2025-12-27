06b7b486e794d76f8536e9b3c25876d98a193cc5
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-27 14:23:50 +0900

    필요없는항목 삭제
    필요없는항목 삭제

--- 빔조립기/model/rail.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass, field
-from .bracket import Bracket
-
-@dataclass
-class Rail:
-    name: str
-    index: int
-    brackets: list[Bracket] = field(default_factory=list)
