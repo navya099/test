6e679f4eb12333d7a264609e5ba19b62e5489a61
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-27 17:14:31 +0900

    Create systaxfactory.py
    BVE 문법 생성 팩토리클래스

---
+++ kmpostcreator/kmpost/systaxfactory.py
@@ -0,0 +1,21 @@
+class KMBVESyntaxFactory:
+    def __init__(self, first_index=4025):
+        self.first_index = first_index
+
+    def create(self, i, station, structure, target_directory):
+        index = self.first_index + i
+        return (
+            self.create_km_index_data(index, station, target_directory),
+            self.create_km_post_data(index, station, structure)
+        )
+
+    @staticmethod
+    def create_km_index_data(idx, sta, work_directory):
+        object_folder = work_directory.split("Object/")[-1]
+        data = f'.freeobj({idx}) {object_folder}/{sta}.csv\n'
+        return data
+
+    @staticmethod
+    def create_km_post_data(idx, sta, struc):
+        data = f'{sta},.freeobj 0;{idx};,;{struc}\n'
+        return data
