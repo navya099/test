efab1bfe414f518c8720059db4651f1a74825574
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-27 14:24:59 +0900

    Update bracket.py
    독스트링 추가

--- 빔조립기/model/bracket.py
+++ 빔조립기/model/bracket.py
@@ -3,6 +3,15 @@ from dataclasses import dataclass
 
 @dataclass
 class Bracket:
+    """브래킷 데이터
+    Attributes:
+        rail_no:선로번호
+        type:브래킷 이름
+        xoffset:X
+        yoffset:Y
+        rotation:회전
+        index: 오브젝트 인덱스
+    """
     rail_no: int
     type: str
     xoffset: float
