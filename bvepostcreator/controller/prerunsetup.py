fbad54863a2494b9be9c4001a376007341bc1f0f
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-28 10:32:03 +0900

    Update prerunsetup.py
    복선 관련 추가
    ask_offset메서드 ->show_input_float로 변경

--- bvepostcreator/controller/prerunsetup.py
+++ bvepostcreator/controller/prerunsetup.py
@@ -16,7 +16,7 @@ class PreRunSetup:
         self.state.isbrokenchain = exists
         self.state.brokenchain = value if exists else 0.0
 
-        offset = self.dialogs.ask_offset()
+        offset = self.dialogs.show_input_float(title='오프셋 입력',prompt='오프셋 값을 입력하세요.')
         if offset is None:
             offset = 0.0
         self.state.offset = offset
@@ -26,4 +26,10 @@ class PreRunSetup:
             return False
         self.state.posttype = posttype
 
+        #복선 여부 확인후 선로간격 입력받기
+        if self.state.is_two_track:
+            track_distance = self.dialogs.show_input_float(title='선로중심간격 입력',prompt='선로 중심간격을 입력하세요.')
+            if track_distance is None:
+                return False
+            self.state.track_distance = track_distance
         return True
