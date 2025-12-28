c2910d486343f6bd08e2f524f39533ee77677de4
navya099 <130723074+navya099@users.noreply.github.com>
2025-12-28 10:29:20 +0900

    Update systaxfactory.py
    복선 지원 추가

--- bvepostcreator/kmpost/systaxfactory.py
+++ bvepostcreator/kmpost/systaxfactory.py
@@ -2,11 +2,11 @@ class KMBVESyntaxFactory:
     def __init__(self, first_index=4025):
         self.first_index = first_index
 
-    def create(self, i, station, structure, target_directory):
+    def create(self, i, station, structure, target_directory, is_two_track):
         index = self.first_index + i
         return (
             self.create_km_index_data(index, station, target_directory),
-            self.create_km_post_data(index, station, structure)
+            self.create_km_post_data(index, station, structure, is_two_track)
         )
 
     @staticmethod
@@ -16,6 +16,17 @@ class KMBVESyntaxFactory:
         return data
 
     @staticmethod
-    def create_km_post_data(idx, sta, struc):
-        data = f'{sta.origin_sta},.freeobj 0;{idx};,;{struc}\n'
-        return data
+    def create_km_post_data(idx, sta, struc, is_two_track):
+        if is_two_track:
+            # 복선
+            data = (
+                f',;구조물={struc}\n'
+                f',;하선\n'
+                f'{sta.origin_sta},.freeobj 0;{idx};\n'
+                f',;상선\n'
+                f'{sta.origin_sta},.freeobj 1;{idx};0;0;180;\n'
+            )
+            return data
+        else:
+            # 단선
+            return f'{sta.origin_sta},.freeobj 0;{idx};,;{struc}\n'
