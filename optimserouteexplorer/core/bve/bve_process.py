4facb581b8e24f4b36dc1d6667ec88505591379d
navya099 <130723074+navya099@users.noreply.github.com>
2026-01-18 01:01:28 +0900

    Update bve_process.py
    bve프로세스 버그수정

--- optimserouteexplorer/core/bve/bve_process.py
+++ optimserouteexplorer/core/bve/bve_process.py
@@ -10,18 +10,22 @@ class BVEProcess:
     def __init__(self, results: dict):
         self.results = results
     def run(self):
-        #top10에서 curve추출
+        self.save_infos()
+        self.create_bve_syntax()
+
+    def save_infos(self):
+        # top10에서 curve추출
         segments = self.results['plan_full']['segments']
         length = self.results['plan_full']['linestring'].length
         pitchs = self.results['profile']['fg_profile']
-        curve_blocks = BVEBlcokExporter.export_curve_info(segments, start_sta=0.0,end_sta=length)
-        pitch_blocks = BVEBlcokExporter.export_pitch_info(pitchs, start_sta=0.0,end_sta=length)
-        #1 curve_info txt저장
+        curve_blocks = BVEBlcokExporter.export_curve_info(segments, start_sta=0.0, end_sta=length)
+        pitch_blocks = BVEBlcokExporter.export_pitch_info(pitchs, start_sta=0.0, end_sta=length)
+        # 1 curve_info txt저장
         BVEInfoSaver.save_curve_info('C:/Temp/curve_info.txt', curve_blocks)
-        #2 pitch_info txt 저장
+        # 2 pitch_info txt 저장
         BVEInfoSaver.save_pitch_info('C:/Temp/pitch_info.txt', pitch_blocks)
-        #3 height info txt 저장
-        design_elevs =  self.results['profile']['design_elevs']
+        # 3 height info txt 저장
+        design_elevs = self.results['profile']['design_elevs']
         ground_elevs = self.results['profile']['ground_elevs']
 
         elevdiff_list = np.array(ground_elevs) - np.array(design_elevs)
@@ -29,14 +33,20 @@ class BVEProcess:
         height_blocks = TerrainGerator.create_terrain(elevdiff_list)
         BVEInfoSaver.save_height_info('C:/Temp/height_info.txt', height_blocks)
 
-        #4 bve_cooridnates 저장
+        # 4 bve_cooridnates 저장
         coords = self.results['plan_full']['tmcoords']
-        BVEInfoSaver.save_cooridnate_info('C:/Temp/bve_coordinates.txt', coords)
+        from core.util import Vector3
+        vec_list = [Vector3(x=c[0], y=c[1], z=0.0) for c in coords]  # z는 필요시 추가
+        BVEInfoSaver.save_cooridnate_info('C:/Temp/bve_coordinates.txt', vec_list)
 
-        #5 구조물xlsx 저장
+        # 5 구조물xlsx 저장
         bridges = self.results['result']['bridges']
-        turnnels  = self.results['result']['turnnels']
+        turnnels = self.results['result']['tunnels']
         structures = StructureCollection()
-        structures.append(bridges)
-        structures.append(turnnels)
+        structures.extend(bridges)  # 리스트 전체를 풀어서 추가
+        structures.extend(turnnels)
+
         StructureSaver.save_to_excel(structures, 'C:/Temp/구조물.xlsx')
+
+    def create_bve_syntax(self):
+        pass
