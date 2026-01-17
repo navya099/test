811cbf5b8ffea26726d9c9ee3b505c5a429fb7cd
navya099 <130723074+navya099@users.noreply.github.com>
2026-01-18 01:00:43 +0900

    메인프로세스 함수 인수 수정

--- optimserouteexplorer/core/run.py
+++ optimserouteexplorer/core/run.py
@@ -2,7 +2,7 @@ from ga.ga import GeneticAlgorithm
 from out.out_ga import format_top10
 
 
-def run_process(start, end, n_candidates, n_generations, top_n):
+def run_process(start, end, n_candidates, n_generations):
     ga = GeneticAlgorithm(start, end, n_candidates, n_generations)
     population = ga.evolve()
     return population
--- optimserouteexplorer/local/localentry.py
+++ optimserouteexplorer/local/localentry.py
@@ -17,21 +17,23 @@ def get_input_points():
             print("입력이 잘못되었습니다. 다시 입력하세요.")
 
 
-def process_routes(start, end):
+def process_routes(start, end, n_candidates=30, n_generations=50):
     """GA 실행 및 결과 반환"""
-    results = run_process(start, end, n_candidates=30, n_generations=50, top_n=10)
+    results = run_process(start, end, n_candidates=n_candidates, n_generations=n_generations)
     return results
 
 
-def format_results(results):
+def format_results(results, top_n=10):
     """상위 10개 결과 포맷팅"""
-    return format_top10(results, top_n=10)
+    return format_top10(results, top_n=top_n)
 
 
 def export_results(top10):
     """결과를 다양한 포맷으로 출력"""
     export_excel_report(top10, "c:/temp/top10_report.xlsx")
-    visualize_routes_with_button(top10, start=None, end=None, top_n=10,
+
+def visulalize(top10, start, end, top_n=10):
+    visualize_routes_with_button(top10, start=start, end=end, top_n=top_n,
                                  map_file="c:/temp/candidate_routes.html")
 
 
@@ -40,14 +42,17 @@ def main():
     start, end = get_input_points()
 
     # 2. 원본 처리
-    results = process_routes(start, end)
+    results = process_routes(start, end, n_candidates=10, n_generations=10)
 
     # 3. 포맷팅
-    top10 = format_results(results)
+    top10 = format_results(results, top_n=10)
 
     # 4. 출력
     export_results(top10)
 
-    # 5. BVE 변환 (필요 시)
+    #5 #시각화
+    visulalize(top10, start, end, top_n=10)
+
+    #6. BVE 변환 (필요 시)
     bve_processor = BVEProcess(results[0])
     bve_processor.run()
