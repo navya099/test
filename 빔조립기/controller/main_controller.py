e88e7f307e03e9d7f65e6898ade5dc7332b5e821
navya099 <qltmxkr@gmail.com>
2026-01-29 16:02:54 +0900

    메인프로세서 간소화
    이미 처리된 dto를 받아서 내부의 어셈블리만 수정하는 프로세스 역할로 수정

--- 빔조립기/controller/main_controller.py
+++ 빔조립기/controller/main_controller.py
@@ -1,41 +1,31 @@
 import pandas as pd
-from scipy.optimize import bracket
 
+from assembler.rail_assembler import RailAssembler
 from controller.library_controller import IndexLibrary
-from controller.poleinstall_controller import PoleInstall
 from model.beam_assembly import BeamAssembly
+from model.pole_install import PoleInstall
+from resolver.bracket_resolver import BracketResolver
 
 SHEET_ID = "1z0aUcuZCxOQp2St3icbQMbOkrSPfJK_8iZ2JKFDbW8c"
 SHEET_NAME = "freeobj"  # ← 원하는 시트 이름
 URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
 
 class MainProcess:
-    def __init__(self, master):
-        self.master = master
+    def __init__(self):
+        self._cached_df = None
 
-    _cached_df = None
-
-    def run(self):
-        if MainProcess._cached_df is None:
-            MainProcess._cached_df = pd.read_csv(URL)
-        df = MainProcess._cached_df
+    def run(self, dto: PoleInstall):
+        if self._cached_df is None:
+            self._cached_df = pd.read_csv(URL)
+        df = self._cached_df
         idxlib = IndexLibrary(df)
 
-        # 설치 시작
-        install = PoleInstall(
-            station=self.master.station.get(),
-            pole_number=self.master.pole_number.get(),
-            railtype=self.master.railtype.get(),
-            rail_count=self.master.rail_count.get(),
-            left_x=self.master.left_x.get(),
-            right_x=self.master.right_x.get(),
-            beam_type=self.master.structure_frame.beam_type.get(),
-            pole_type=self.master.structure_frame.pole_type.get(),
-            pole_width=self.master.structure_frame.pole_width.get(),
-            pole_height=self.master.structure_frame.pole_height.get(),
-            pole_count=2
-        )
-        install.brackets = self.master.bracket_frame.bracket_vars
-        install.beam = BeamAssembly.create_from_install(install, idxlib)
-        return install
+        # 🔹 여기서 bracket 보정
+        all_brackets = [
+            b for rail in dto.rails for b in rail.brackets
+        ]
+        BracketResolver.resolve(all_brackets, idxlib)
+
+        dto.beam_assembly = BeamAssembly.create_from_install(dto, idxlib)
+        return dto
 
