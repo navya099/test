4d6fdfef7fcb485176c11bf09a0f16e6f45ea57a
navya099 <130723074+navya099@users.noreply.github.com>
2026-01-29 22:17:18 +0900

    Update bveautosolution.py
    구조물표 제외 완성

--- 스크립트/bveautosolution.py
+++ 스크립트/bveautosolution.py
@@ -1,10 +1,15 @@
+import time
+
 from bve곡선레일구문생성기 import preprocess_input_index, extract_cant, create_freeobj, \
     cal_mainlogic, save_files, create_railtype, save_railtype
 from 거리표이미지생성기 import select_target_directory, copy_all_files, create_km_object, apply_brokenchain_to_structure
+from 곡선표이미지생성기 import process_curve_data, create_curve_post_txt, create_curve_index_txt, create_speed_limit
 from 교량터널구문생성기 import get_designspeed, get_linecount, load_structure_data, create_network_data, create_txt, \
     find_structure_section
 import os
 import random
+
+from 기울기표이미지생성기 import process_bve_profile, create_pitch_post_txt, create_pitch_index_txt
 from 랜덤신호위치생성 import save_signal_data
 from 랜덤전주위치생성 import distribute_pole_spacing_flexible, load_curve_data, load_pitch_data, \
     load_coordinates, define_airjoint_section, generate_postnumbers, save_pole_data, save_wire_data
@@ -24,7 +29,7 @@ class SolutionRunner:
         self.brokenchain = None
         self.last_block = None
         self.start_blcok = None
-        self.base_source_directory = 'c:/temp/km_post/소스/'  # 원본 소스 기본 경로
+        self.base_source_directory = 'c:/temp/'  # 원본 소스 기본 경로
         self.structure_list = None
         self.linecount = None
         self.logger = logger
@@ -32,6 +37,8 @@ class SolutionRunner:
         self.railtype_map = {120: '도시철도', 150: '일반철도', 250: '준고속철도', 350: '고속철도'}
         self.alignment_type = ''
         self.path = r'D:/BVE/루트/Railway/Route/연습용루트/'
+        self.obj_path = r'D:\BVE\루트\Railway\Object\abcdefg/'
+
     def log(self, msg):
         self.logger(msg)
 
@@ -73,8 +80,8 @@ class SolutionRunner:
         self.log(f' 시작 측점 : {self.start_blcok}, 끝 측점 : {self.last_block}')
 
 
-        self.brokenchain = get_float_input('파정 입력 : ')
-        self.offset = get_float_input('오프셋 입력 : ')
+        self.brokenchain = 0.0
+        self.offset = 0.0
 
         self.log(f'파정 : {self.brokenchain}, freeobj x 오프셋 : {self.offset}')
         self.log("기본 정보 로드 완료")
@@ -93,7 +100,7 @@ class SolutionRunner:
     def run_curve(self):
         self.log("곡선레일 생성 시작")
         try:
-            index_dict = preprocess_input_index(self.alignment_type)
+            index_dict = preprocess_input_index(self.alignment_type, mode='default')
             self.log("파일을 읽는 중...")
 
             self.log("캔트 정보 추출 중...")
@@ -104,12 +111,14 @@ class SolutionRunner:
 
             self.log("FreeObj 생성 중...")
             content = create_freeobj(freeobj, self.structure_list, self.curve_info, index_dict)
-            save_files(content)
+            save_path = os.path.join(self.path, 'freeobj.txt')
+            save_files(content, save_path)
             self.log("FreeObj 저장 완료!")
 
             self.log("Railtype 생성 중...")
             railtype = create_railtype(self.curve_info, self.structure_list, index_dict)
-            save_railtype(railtype)
+            save_path = os.path.join(self.path, 'railtype.txt')
+            save_railtype(railtype, save_path)
             self.log("Railtype 저장 완료!")
 
             self.log("FreeObj 및 RailType 생성이 완료되었습니다.")
@@ -190,6 +199,7 @@ class SolutionRunner:
 
     def create_km_post(self):
         self.log('거리표 생성 시작')
+
         try:
             # 디렉토리 설정
             self.log("작업 디렉토리 확인 중...")
@@ -202,19 +212,14 @@ class SolutionRunner:
 
             # 대상 디렉토리 선택
             self.log("대상 디렉토리 선택 중...")
-            self.target_directory = select_target_directory()
+            self.target_directory = os.path.join(self.obj_path, 'km_post')
             self.log(f"대상 디렉토리: {self.target_directory}")
 
             #노선 종류 입력받기
 
             # ✅ 항상 base_source_directory에서 새로 경로 만들기
-            self.source_directory = os.path.join(self.base_source_directory, self.alignment_type) + '/'
+            self.source_directory = os.path.join(self.base_source_directory,'km_post/소스/', self.alignment_type) + '/'
             self.log(f"소스 경로: {self.source_directory}")
-
-
-
-            # 구조물 측점 파정처리
-            self.structure_list = apply_brokenchain_to_structure(self.structure_list, self.brokenchain)
 
             intervel = 100 if self.alignment_type == '도시철도' else 200
             self.log("KM Object 생성 중...")
@@ -243,19 +248,107 @@ class SolutionRunner:
             self.log(f"작업 중 오류가 발생했습니다:\n{e}")
 
     def create_curve_post(self):
-        pass
+        self.log('곡선표 생성 시작')
+        try:
+            # 디렉토리 설정
+            self.log("작업 디렉토리 확인 중...")
+            self.work_directory = 'c:/temp/curve/result/'
+            if not os.path.exists(self.work_directory):
+                os.makedirs(self.work_directory)
+                self.log(f"디렉토리 생성: {self.work_directory}")
+            else:
+                self.log(f"디렉토리 존재: {self.work_directory}")
+
+            # 대상 디렉토리 선택
+            self.log("대상 디렉토리 선택 중...")
+            self.target_directory = os.path.join(self.obj_path, 'curve_post')
+            self.log(f"대상 디렉토리: {self.target_directory}")
+
+            # ✅ 항상 base_source_directory에서 새로 경로 만들기
+            self.source_directory = os.path.join(self.base_source_directory,'curve/소스/', self.alignment_type) + '/'
+            self.log(f"소스 경로: {self.source_directory}")
+
+            # 곡선 정보 파일 읽기
+            self.log("곡선 정보 파일 읽는 중...")
+
+            # 곡선 데이터 처리
+            self.log("곡선 데이터 처리 중...")
+            objectdatas = process_curve_data(self.source_directory, self.work_directory, self.target_directory, self.curve_info,
+                                             self.structure_list, self.brokenchain, self.alignment_type, 'BVE', self.offset)
+
+            # 최종 텍스트 생성
+            if objectdatas:
+                self.log("최종 결과 생성 중...")
+                create_curve_post_txt(objectdatas, self.work_directory)
+                create_curve_index_txt(objectdatas, self.work_directory)
+                create_speed_limit(objectdatas, self.work_directory)
+                self.log("결과 파일 생성 완료!")
+
+            # 파일 복사
+            self.log("결과 파일 복사 중...")
+            copy_all_files(self.work_directory, self.target_directory, ['.csv', '.png', '.txt'], ['.dxf', '.ai'])
+
+            self.log("✅ 모든 작업이 성공적으로 완료되었습니다.")
+
+
+        except Exception as e:
+            self.log(f"[오류] {str(e)}")
     def create_pitch_post(self):
-        pass
+        self.log('기울기표 생성 시작')
+        try:
+            # 디렉토리 설정
+            self.log("작업 디렉토리 확인 중...")
+            self.work_directory = 'c:/temp/pitch/result/'
+            if not os.path.exists(self.work_directory):
+                os.makedirs(self.work_directory)
+                self.log(f"디렉토리 생성: {self.work_directory}")
+            else:
+                self.log(f"디렉토리 존재: {self.work_directory}")
+
+            # 대상 디렉토리 선택
+            self.log("대상 디렉토리 선택 중...")
+            self.target_directory = os.path.join(self.obj_path, 'pitch_post')
+            self.log(f"대상 디렉토리: {self.target_directory}")
+
+            # ✅ 항상 base_source_directory에서 새로 경로 만들기
+            self.source_directory = os.path.join(self.base_source_directory,'pitch/소스/', self.alignment_type) + '/'
+            self.log(f"소스 경로: {self.source_directory}")
+
+            from 기울기표이미지생성기 import process_and_save_sections
+            vipdatas = process_and_save_sections(self.pitch_info, self.brokenchain, 'BVE', self.pitch_info)
+
+            objectdatas = process_bve_profile(vipdatas, self.structure_list, self.source_directory, self.work_directory,
+                                              self.target_directory,self.alignment_type, offset=self.offset)
+
+            # 최종 텍스트 생성
+            if objectdatas:
+                self.log("최종 결과 생성 중...")
+                create_pitch_post_txt(objectdatas, self.work_directory)
+                create_pitch_index_txt(objectdatas, self.work_directory)
+                self.log("결과 파일 생성 완료!")
+            self.log("BVE 작업 완료")
+
+            copy_all_files(self.work_directory, self.target_directory, ['.csv', '.png', '.txt'], ['.dxf', '.ai'])
+            self.log("모든 작업이 완료되었습니다.")
+
+            self.log( "Pitch 데이터 처리가 성공적으로 완료되었습니다.")
+
+        except Exception as e:
+            self.log( f"처리 중 오류가 발생했습니다:\n{e}")
+
     def create_structure_post(self):
         pass
 
 if __name__ == '__main__':
     runner = SolutionRunner()
+    start = time.time()
     runner.load_info()       # 기본 정보 로드
-    #runner.run_bridge()      # 교량/터널 구문 생성
-    #runner.run_curve()       # 곡선 레일 생성
-    #runner.run_pole()        # 전차선 생성
-    #runner.run_signal()      # 신호 생성
-    #runner.run_transmission()  # 통신 생성
+    runner.run_bridge()      # 교량/터널 구문 생성
+    runner.run_curve()       # 곡선 레일 생성
+    runner.run_pole()        # 전차선 생성
+    runner.run_signal()      # 신호 생성
+    runner.run_transmission()  # 통신 생성
     runner.run_post()        # 포스트 생성
-    runner.log("전체 작업 완료")
+    end = time.time()
+    elapsed = end - start
+    runner.log(f"전체 작업 완료! 소요시간 : {elapsed:.2f}초")
