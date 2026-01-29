31ece011c2eb53b6c17f0ab458b68ffa776062c3
navya099 <qltmxkr@gmail.com>
2026-01-29 16:00:23 +0900

    Create bracket_input.py

---
+++ 빔조립기/gui/viewmodel/bracket_input.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+import tkinter as tk
+
+@dataclass
+class BracketViewModel:
+    """UI전용 브래킷 스테이트 보관"""
+    rail_no: tk.IntVar
+    bracket_type: tk.StringVar
+    xoffset: tk.DoubleVar
+    yoffset: tk.DoubleVar
+    rotation: tk.DoubleVar
+    rail_type: tk.StringVar
