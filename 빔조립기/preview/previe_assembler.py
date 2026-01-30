227b270391a2a9a01ea80913d33ba126b34724a5
navya099 <qltmxkr@gmail.com>
2026-01-30 09:42:50 +0900

    피봇 좌표계 변환 추가
    기존 bve좌표계에서 world좌표계로 변환해야함

--- 빔조립기/preview/previe_assembler.py
+++ 빔조립기/preview/previe_assembler.py
@@ -21,13 +21,15 @@ class PreviewAssembler:
             ).parse()
             ## 좌표계는 항상 WORLD 기준으로 ObjectModifier 적용
             coordsystemconvertor.convert(csvobj, CoordinateSystem.WORLD)
+            #피봇 좌표계 변경
+            pivot = coordsystemconvertor.convert_point(item.transform.pivot, CoordinateSystem.WORLD)
             modifier = ObjectModifier(csvobj)
-            modifier.set_pivot(item.transform.pivot)
+            modifier.set_pivot(pivot)
             #필수: 월드좌표로 배치;
             modifier.translate_world(
-                item.transform.pivot.x,
-                item.transform.pivot.y,
-                item.transform.pivot.z
+                pivot.x,
+                pivot.y,
+                pivot.z
             )
             modifier.rotate_z(item.transform.rotation)
             modifier.translate_local(
--- 빔조립기/world/convert.py
+++ 빔조립기/world/convert.py
@@ -26,3 +26,23 @@ class CoordinateConverter:
             ]
 
         csvobj.coordsystem = target
+
+    # -----------------------------
+    # 단일 점 변환
+    # -----------------------------
+    def convert_point(self, point, target: CoordinateSystem):
+        """
+        point: Vector3 (현재 좌표계)
+        target: 변환할 목표 좌표계
+        return: Vector3 (target 좌표계)
+        """
+        # 기본 좌표계는 OPENBVE 기준이라면, point.coordsystem 필요 없으면 csvobj.coordsystem 사용 가능
+        # 여기서는 point가 현재 transform 좌표계라고 가정
+        # 임시 src_tf 지정
+        # 필요 시 인자로 src 좌표계 받도록 확장 가능
+        src_tf = self._map[CoordinateSystem.OPENBVE]  # 현재 pivot이 SketchUp/OpenBVE 기준이라면
+        dst_tf = self._map[target]
+
+        world_v = src_tf.to_world(point)  # 현재 좌표계 → WORLD
+
+        return world_v
