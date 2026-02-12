7289a88570bfed48dc96a21b806751e833b9f659
navya099 <130723074+navya099@users.noreply.github.com>
2026-02-12 21:32:07 +0900

    UI에서 전주 이동 구현
    +-5M씩 버튼을 클릭해서 이동
    SPAN 범위 탐지 예외있음

--- 스크립트/랜덤전주위치생성.py
+++ 스크립트/랜덤전주위치생성.py
@@ -1494,9 +1494,6 @@ class AutoPoleApp(tk.Tk):
 
         main_frame.add(plotter_frame)
 
-        # 이벤트 바인딩
-        self.events.bind("pole_selected", self.plotter.highlight_pole)
-
     def exit_app(self):
         self.quit()
         self.destroy()
@@ -1551,6 +1548,11 @@ class AutoPoleEditor(tk.Frame):
         super().__init__(master)
         self.runner = runner
         self.events = events
+        if self.events:
+            # pole_moved 이벤트가 발생하면 on_pole_moved 실행
+            self.events.bind('pole_moved', self.on_pole_moved)
+
+
         self.editable_poles = []
 
         # Treeview + Scrollbar 프레임
@@ -1620,6 +1622,9 @@ class AutoPoleEditor(tk.Frame):
         tk.Button(self, text="선택 불러오기", command=self.load_selected).pack()
         tk.Button(self, text="수정 저장", command=self.save_edit).pack()
 
+    def on_pole_moved(self):
+        self.refresh_tree()
+
     def refresh_tree(self):
         if not self.runner.poledata:
             return
@@ -1634,8 +1639,15 @@ class AutoPoleEditor(tk.Frame):
 
             item = self.tree.item(selected[0])
             post_number, pos, span, gauge, structure, section, base_type, radius, pitch, cant ,z = item["values"]
-            if self.events:
-                self.events.emit("pole_selected", pos)
+            # epole 객체 찾기
+            epole = None
+            for e in self.editable_poles:
+                if e.pole.pos == pos:
+                    epole = e
+                    break
+
+            if self.events and epole:
+                self.events.emit("pole_selected", epole)
 
             self.original_pos = pos
             self.entry_post_number.delete(0, tk.END)
@@ -1825,8 +1837,14 @@ class Transaction:
 class PlotPoleMap(tk.Frame):
     def __init__(self, runner, events=None, master=None):
         super().__init__(master)
+        self.selected_epole = None
+        self.selected_pole_scatter = None
+        self.sel_xy = None
+        self.selected_pole = None
         self.runner = runner
         self.events = events
+        if self.events:
+            self.events.bind("pole_selected", self.on_pole_selected)
 
         # Matplotlib Figure/Axes 생성
         plt.rcParams['font.family'] = 'Malgun Gothic'
@@ -1841,52 +1859,105 @@ class PlotPoleMap(tk.Frame):
         toolbar.update()
         toolbar.pack(side="bottom", fill="x")
 
+        # 이동 버튼 등록
+        btn_left = tk.Button(self, text="-5m", command=self.move_left)
+        btn_right = tk.Button(self, text="+5m", command=self.move_right)
+        btn_left.pack(side="left")
+        btn_right.pack(side="right")
 
-    def update_plot(self):
-        self.ax.clear()
+    def draw_alignment(self):
         # polyline 좌표 꺼내기
         sta_list, x_list, y_list, z_list = zip(*self.runner.polyline_with_sta)
-
         # 선형 그리기 (예: x vs z)
         self.ax.plot(x_list, y_list, color="gray", label="선형")
 
+    def draw_poles(self):
         # 전주 위치(pos)를 x축에 표시
         poles = self.runner.poledata
         x_val = [pole.coord[0] for pole in poles]
         y_val = [pole.coord[1] for pole in poles]
-
         self.ax.scatter(x_val, y_val, c="blue", marker="o", label="전주")
-        self.ax.set_title("전주 위치 맵")
-        self.ax.set_xlabel("측점 (pos)")
-        self.ax.set_ylabel("계획고 (z)")
-        self.ax.legend()
 
+    def update_plot(self):
+        self.ax.clear()
+        self.ax.set_title("전주 위치 맵")
+        self.ax.set_xlabel("X좌표")
+        self.ax.set_ylabel("Y좌표")
+
+        self.draw_alignment()
+        self.draw_poles()
         self.canvas.draw()
 
     def highlight_pole(self, pos):
+        self.ax.set_title("선택된 전주")
         self.ax.clear()
-        sta_list, x_list, y_list, z_list = zip(*self.runner.polyline_with_sta)
-        self.ax.plot(x_list, y_list, color="gray", label="선형")
-
+        self.draw_alignment()
+        self.draw_poles()
         poles = self.runner.poledata
-        x_val = [pole.coord[0] for pole in poles]
-        y_val = [pole.coord[1] for pole in poles]
-        self.ax.scatter(x_val, y_val, c="blue", marker="o", label="전주")
-
         # 선택된 전주 강조
         for pole in poles:
             if pole.pos == pos:
-                sel_x, sel_y = pole.coord[0], pole.coord[1]
-                self.ax.scatter(sel_x, sel_y, c="red", s=100, label="선택된 전주")
-                self.ax.set_xlim(sel_x - 50, sel_x + 50)
-                self.ax.set_ylim(sel_y - 50, sel_y + 50)
+                self.selected_pole = pole
+                self.sel_xy = pole.coord[0], pole.coord[1]
+
+                self.selected_pole_scatter = self.ax.scatter(self.sel_xy[0], self.sel_xy[1], c="red", s=100, label="선택된 전주", picker=5)
+                self.ax.text(self.sel_xy[0], self.sel_xy[1], f'{pole.post_number}')
+                self.ax.set_xlim(self.sel_xy[0] - 50, self.sel_xy[0] + 50)
+                self.ax.set_ylim(self.sel_xy[1] - 50, self.sel_xy[1] + 50)
+                break
 
         self.ax.legend()
         self.canvas.draw()
 
+    def on_pole_selected(self, epole):
+        self.selected_epole = epole
+        self.highlight_pole(epole.pole.pos)
 
+    def move_left(self):
+        self._move_pole(-5)
+
+    def move_right(self):
+        self._move_pole(+5)
+
+    def _move_pole(self, delta):
+        if not self.selected_epole: return
+
+        new_pos = self.selected_epole.pole.pos + delta
+        prev_pos = self.selected_epole.prev_pole.pole.pos if self.selected_epole.prev_pole else None
+        next_pos = self.selected_epole.next_pole.pole.pos if self.selected_epole.next_pole else None
+
+        # 범위 검사
+        if prev_pos and new_pos <= prev_pos: return
+        if next_pos and new_pos >= next_pos: return
+        #타입ㄱ머사
+        if self.selected_epole.pole.section is not None:
+            messagebox.showerror('오류', f'{self.selected_epole.pole.post_number}: 지정한 전주는 일반개소가 아닙니다. 일반개소만 이동이 가능합니다.')
+            return
+        #span 검사
+        new_prev_pole_span = new_pos - prev_pos #이전전주와의 span
+        new_span = next_pos - new_pos #다음 전주와의 스판
+
+        if new_prev_pole_span not in self.runner.dataprocessor.get_span_list() or new_span not in self.runner.dataprocessor.get_span_list():
+            messagebox.showerror('오류', f'{self.selected_epole.pole.post_number}: 지정한 전주는 span 범위에 없습니다.')
+            return
+
+        # 보간 좌표 계산
+        coord, _, v1 = interpolate_cached(self.runner.polyline_with_sta, new_pos)
+        pos_coord_with_offset = calculate_offset_point(v1, coord, self.selected_epole.pole.gauge)
+
+        # 최종 반영
+        with Transaction(self.selected_epole.pole,
+                         self.selected_epole.prev_pole.pole if self.selected_epole.prev_pole else None,
+                         self.selected_epole.next_pole.pole if self.selected_epole.next_pole else None):
+            self.selected_epole.update(pos=new_pos)
+
+        self.selected_epole.pole.coord = pos_coord_with_offset
+        self.runner.wire_data = self.runner.wire_processor.process_to_wire()
+        self.runner.log(f"전주 {self.selected_epole.pole.post_number} 이동 완료: {new_pos}")
+
+        self.highlight_pole(new_pos)
+        self.events.emit('pole_moved') #전주 이동 이벤트 발생
 # 실행
 if __name__ == "__main__":
-
     app = AutoPoleApp()
     app.mainloop()
