9d21ac306ee707b27c9b18a2e3197c046444d00d
navya099 <130723074+navya099@users.noreply.github.com>
2026-02-14 23:45:22 +0900

    메인러너에서 다중트랙 지원을 위한 구조변경

--- DeCATSBVE/core/runner.py
+++ DeCATSBVE/core/runner.py
@@ -9,9 +9,12 @@ from file_io.filemanager import read_file, load_structure_data, load_curve_data,
     write_to_file
 from utils.comom_util import find_last_block, distribute_pole_spacing_flexible, define_airjoint_section, \
     generate_postnumbers
+from shapely.geometry.linestring import LineString
 
 class AutoPole:
     def __init__(self, log_widget):
+        self.offset_line_with_25 = None
+        self.pole_positions = None
         self.idxlib = None
         self.airjoint_list = None
         self.pitchlist = None
@@ -27,6 +30,9 @@ class AutoPole:
         self.designspeed = 0
         self.iscustommode = False
         self.is_create_dxf = False
+        self.track_mode = None
+        self.track_direction = None
+        self.track_distance = None
         self.log_widget = log_widget
         self.poledata = None
         self.wire_data = None
@@ -69,16 +75,59 @@ class AutoPole:
 
         #데이터셋 로드,
         dataset = load_dataset(self.designspeed, self.iscustommode)
-        spans, pole_positions = distribute_pole_spacing_flexible(start_km, end_km, spans=dataset['span_list'])
-        self.airjoint_list = define_airjoint_section(pole_positions ,airjoint_span=1600)
+        spans, self.pole_positions = distribute_pole_spacing_flexible(start_km, end_km, spans=dataset['span_list'])
+        self.airjoint_list = define_airjoint_section(self.pole_positions ,airjoint_span=1600)
 
         # 전주번호 추가
-        post_number_lst = generate_postnumbers(pole_positions)
+        post_number_lst = generate_postnumbers(self.pole_positions)
         # 데이터 처리
         self.dataprocessor = DatasetGetter(dataset)
 
+        ######트랙별 처리###### todo
+        #현재는 두 트랙이 동일하다고 가정(즉 pos와 모든 구조물을 공유함)
+        #선형만 별도로 존재(offset 적용)
+        #선형 오프셋
+
+        # LineString 생성
+
+        line = LineString(polyline)
+        # 오프셋 적용
+        if self.track_direction == "본선 L / 상선 R":
+            direction = 'right'
+        else:
+            direction = 'left'
+        offset_line = line.parallel_offset(abs(self.track_distance), direction, join_style=2)
+        #다시 분해후 재결합
+        offset_polyline = offset_line.coords
+        self.offset_line_with_25 = [(i * 25, *values) for i, values in enumerate(offset_polyline)]
+        positions_by_track = {
+            "main": self.pole_positions,
+            "sub": self.pole_positions  # double일 때만
+        }
+        structure_list_by_track = {
+            "main": self.structure_list,
+            "sub": self.structure_list
+        }
+        curve_list_by_track = {
+            "main": self.curvelist,
+            "sub": self.curvelist
+        }
+        pitchlist_by_track = {
+            "main": self.pitchlist,
+            "sub": self.pitchlist
+        }
+        alignment_by_track = {
+            "main": self.polyline_with_sta,
+            "sub": self.offset_line_with_25
+        }
         self.pole_processor = PoleProcessor()
-        self.poledata = self.pole_processor.process_pole(pole_positions, self.structure_list, self.curvelist, self.pitchlist, self.dataprocessor, self.airjoint_list, self.polyline_with_sta, self.idxlib)
+
+        self.poledata = self.pole_processor.process_pole_multitrack(
+            positions_by_track, structure_list_by_track, curve_list_by_track, pitchlist_by_track,
+            self.dataprocessor, self.airjoint_list, alignment_by_track, self.idxlib,
+            self.track_mode, self.track_direction,
+        )
+
         self.wire_processor = WireProcessor(self.dataprocessor, self.polyline_with_sta, self.poledata)
         self.wire_data = self.wire_processor.process_to_wire()
         self.pole_path = asksaveasfilename(title='전주 데이터 저장')
