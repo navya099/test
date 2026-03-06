5728248c46bc1291996d7b867229f88e47d9b407
navya099 <qltmxkr@gmail.com>
2026-03-06 17:52:56 +0900

    전주 치수 테이블 매퍼 추가

--- 빔조립기/utils/pole_dimention_finder.py
+++ 빔조립기/utils/pole_dimention_finder.py
@@ -1,7 +1,7 @@
 class PoleDimensionFinder:
     @staticmethod
     def find_pole_dimension(pole):
-        pole_dimension_tables = {
+        pipepole_dimension_tables = {
             'P10': 0.2674,
             'P12': 0.3285,
             'P14': 0.3556,
@@ -9,4 +9,22 @@ class PoleDimensionFinder:
             'P18': 0.4572,
             'P20': 0.5
         }
-        return pole_dimension_tables[pole.width]
+        hbeam_pole_dimension_tables = {
+        'H250x250': 0.25,
+            'H300x300': 0.3
+        }
+        steel_pole_dimension_tables = {
+            'L75x300x400': 0.3,
+            'L75x450x450': 0.45,
+            'L90x450x450': 0.45
+        }
+
+        # 타입별 딕셔너리 매핑
+        pole_dimension_tables = {
+            'PIPE': pipepole_dimension_tables,
+            'H_BEAM': hbeam_pole_dimension_tables,
+            'STEEL': steel_pole_dimension_tables
+        }
+
+        # pole.type에 따라 해당 딕셔너리에서 width 키를 찾아 반환
+        return pole_dimension_tables[pole.type.name][pole.width]
