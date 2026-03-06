d2812b2a435813e2ff162d5ccc0bf7473e31989d
navya099 <qltmxkr@gmail.com>
2026-03-06 17:51:53 +0900

    전주 커스텀 판별로직 수정
    강관주 외 조립철주 h빔 지원 추가

--- 빔조립기/resolver/pole_resolver.py
+++ 빔조립기/resolver/pole_resolver.py
@@ -1,3 +1,4 @@
+from Electric.Overhead.Pole.poletype import PoleType
 from model.rail import RailData
 from utils.pole_dimention_finder import PoleDimensionFinder
 from utils.polenamer import PoleNameBuilder
@@ -6,6 +7,7 @@ from utils.polenamer import PoleNameBuilder
 class PoleResolver:
     @staticmethod
     def resolve(poles, idxlib, rail_map):
+        standard_lengths = [7, 7.5, 8, 8.5, 9]
         for pole in poles:
             name = PoleNameBuilder.build(pole)
             index = idxlib.get_index(name)
@@ -13,7 +15,10 @@ class PoleResolver:
             pole.display_name = name
             pole.obj_index = index
             pole.base_rail = rail_map[pole.base_rail_index]
-            if pole.length in [7,7.5,8,8.5,9]:
+            # 조건 세분화
+            if (pole.type == PoleType.PIPE) and (pole.length in standard_lengths):
+                # PIPE 타입이고 표준 길이 → 기본 폴
                 pole.iscustom = False
             else:
+                # 그 외 모든 경우 → 커스텀 모드 필요
                 pole.iscustom = True
