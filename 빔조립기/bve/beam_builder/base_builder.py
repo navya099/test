9853b2712e0982eb5e4090a5f8f59b662e3cd146
navya099 <130723074+navya099@users.noreply.github.com>
2026-03-06 23:16:52 +0900

    Update base_builder.py
    개행문자 추가

--- 빔조립기/bve/beam_builder/base_builder.py
+++ 빔조립기/bve/beam_builder/base_builder.py
@@ -26,25 +26,25 @@ class BaseBeamBuilder(ABC):
     def v(self, x, y, z, nx, ny, nz):
         """버텍스 생성"""
         self.text.append(
-            f'AddVertex, {x:.6f}, {y:.6f}, {z:.6f}, {nx}, {ny}, {nz},'
+            f'AddVertex, {x:.6f}, {y:.6f}, {z:.6f}, {nx}, {ny}, {nz},\n'
         )
 
     def addface(self, idx1, idx2, idx3, isface2=False):
         """페이스 생성"""
         face = 'AddFace' if not isface2 else 'AddFace2'
         self.text.append(
-            f'{face}, {idx1}, {idx2}, {idx3},'
+            f'{face}, {idx1}, {idx2}, {idx3},\n'
         )
 
     def load_texture(self, texturefile_name):
         self.text.append(
-            f'LoadTexture, {texturefile_name},'
+            f'LoadTexture, {texturefile_name},\n'
         )
-    def set_color(self, r=0, g=0, b=0):
+    def set_color(self, r=0, g=0, b=0,a=0):
         self.text.append(
-            f'SetColor, {r}, {g}, {b},'
+            f'SetColor, {r}, {g}, {b}, {a},\n'
         )
     def set_texture_coordinates(self, idx, u, v):
         self.text.append(
-            f'SetTextureCoordinates, {idx}, {u}, {v},'
+            f'SetTextureCoordinates, {idx}, {u}, {v},\n'
         )
