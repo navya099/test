9f869edc5ebcd281e73277274134933fd37bbbdc
navya099 <130723074+navya099@users.noreply.github.com>
2026-03-07 13:07:37 +0900

    Update bve_structure_list.py
    기둥 인덱스 추가

--- 빔조립기/bve/bve_structure_list.py
+++ 빔조립기/bve/bve_structure_list.py
@@ -1,5 +1,5 @@
 from resolver.beam_resolver import BeamResolver
-
+from resolver.pole_resolver import PoleResolver
 
 class BVEStrurctureList:
     @staticmethod
@@ -8,4 +8,10 @@ class BVEStrurctureList:
         for index, name in BeamResolver.index_dic.items():
             line = f".freeobj({index}) 동해선\\전차선\\월포정거장\\빔\\{name}.csv"
             lines.append(line)
+
+        lines.append(",; 커스텀 기둥 목록 =====================")
+        for index, name in PoleResolver.index_dic.items():
+            line = f".freeobj({index}) 동해선\\전차선\\월포정거장\\기둥\\{name}.csv"
+            lines.append(line)
+
         return "\n".join(lines)
