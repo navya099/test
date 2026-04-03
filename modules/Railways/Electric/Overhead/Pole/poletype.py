ab16a61833989830392f916d4ee80a958178ea11
navya099 <130723074+navya099@users.noreply.github.com>
2026-04-03 10:47:31 +0900

    pole타입 빔타입 패치

--- modules/Railways/Electric/Overhead/Pole/poletype.py
+++ modules/Railways/Electric/Overhead/Pole/poletype.py
@@ -9,14 +9,6 @@ class PoleType(Enum):
         H_BEAM: H형강주
         STEEL: 조립철주
     """
-    PIPE = 0
-    H_BEAM = 1
-    STEEL = 2
-
-    @property
-    def label(self) -> str:
-        return {
-            PoleType.PIPE: "강관주",
-            PoleType.H_BEAM: "H형강주",
-            PoleType.STEEL: "조립철주",
-        }[self]
+    PIPE = '강관주'
+    H_BEAM = 'H형강주'
+    STEEL = '조립철주'
--- modules/Railways/Electric/Overhead/Structure/beamtype.py
+++ modules/Railways/Electric/Overhead/Structure/beamtype.py
@@ -10,16 +10,7 @@ class BeamType(Enum):
         TRUSS: 4각트러스
         TRUSS_RHAMEN: 4각트러스라멘
     """
-    PIPE = 0
-    H_BEAM = 1
-    TRUSS = 2
-    TRUSS_RHAMEN = 3
-
-    @property
-    def label(self) -> str:
-        return {
-            BeamType.PIPE: "깅관빔",
-            BeamType.H_BEAM: "H형강빔",
-            BeamType.TRUSS: "트러스빔",
-            BeamType.TRUSS_RHAMEN: "트러스라멘빔",
-        }[self]
+    PIPE = '강관빔'
+    H_BEAM = 'H형강빔'
+    TRUSS = '트러스빔'
+    TRUSS_RHAMEN = '트러스라멘빔'
