63f399e9b83c28c130ec78dce3151311fb01c866
navya099 <130723074+navya099@users.noreply.github.com>
2026-04-18 14:04:53 +0900

    절대경로를 상대경로 UI로 불러오도록 수정

--- 루비/좌표를 읽고 폴리선 그리기.rb
+++ 루비/좌표를 읽고 폴리선 그리기.rb
@@ -5,12 +5,22 @@ def meters_to_inches(meters)
   meters * inches_per_meter
 end
 
-def draw_polyline_from_csv(csv_path)
+def draw_polyline_from_csv
+  # UI에서 파일 선택
+  csv_path = UI.openpanel("CSV 파일 선택", "c:/temp", "CSV 파일|*.txt||")
+  return unless csv_path # 사용자가 취소하면 종료
+
   # CSV 파일에서 좌표를 읽어옵니다.
   points = read_points_from_csv(csv_path)
 
   # 좌표 단위를 미터에서 인치로 변환합니다.
-  points = points.map { |point| Geom::Point3d.new(meters_to_inches(point.x), meters_to_inches(point.y), meters_to_inches(point.z)) }
+  points = points.map do |point|
+    Geom::Point3d.new(
+      meters_to_inches(point.x),
+      meters_to_inches(point.y),
+      meters_to_inches(point.z)
+    )
+  end
 
   # 좌표를 따라 폴리라인을 그립니다.
   draw_polyline(points)
@@ -31,5 +41,5 @@ def draw_polyline(points)
   Sketchup.active_model.entities.add_edges(points)
 end
 
-#코드 실행
-draw_polyline_from_csv('c:/temp/coord.csv')
+# 코드 실행
+draw_polyline_from_csv
