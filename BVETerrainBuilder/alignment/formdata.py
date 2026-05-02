418f61fe5107cfe941533aabf3bde492fef1c1f1
navya099 <130723074+navya099@users.noreply.github.com>
2026-05-02 11:15:14 +0900

    레포지토리 분리후 제거

--- BVETerrainBuilder/alignment/formdata.py
+++
@@ -1,7 +0,0 @@
-from .form import Form
-
-class FormData:
-    def __init__(self, name: str):
-        self.name = name
-
-        self.formdata: list[Form] = []
--- BVETerrainBuilder/main.py
+++
@@ -1,5 +0,0 @@
-from app import MainAPP
-
-if __name__ == '__main__':
-    app = MainAPP(is_cui=False, debug=False)
-    app.run()
